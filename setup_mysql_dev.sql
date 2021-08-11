131dcf5200456f0a2d0e4a26ba8903e55cb9af51
gavazcal <2392@holbertonschool.com>
2021-08-11 17:01:48 +0000

    fixed typo

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grants privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnd_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grants privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
