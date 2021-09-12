f8b693bc5b6cf3be4d9d83f7c265b782f17ef1ff
gavazcal <2392@holbertonschool.com>
2021-09-12 16:11:13 +0000

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python3
+"""
+starts Flask
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Prints output"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
