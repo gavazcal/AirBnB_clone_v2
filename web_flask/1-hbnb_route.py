d0d3d86b4d69b495b1d8d0e1bce434aad745023a
gavazcal <2392@holbertonschool.com>
2021-09-12 16:21:27 +0000

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+starts Flask
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Prints output"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def helloagain()
+    """Other hello"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
